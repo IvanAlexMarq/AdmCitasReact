95ef87624f314589de4fe52130e721ed98646fa1
IvanAlexMarq <70303520+IvanAlexMarq@users.noreply.github.com>
2020-10-23 10:09:55 -0400

    local storage agregando y quitando

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Cita from './components/Cita';
 
 function App() {
 
+  //Citas en local Storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if (!citasIniciales) {//si citasIniciales es null o vacio
+    citasIniciales = [];
+  }
+
   //state Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   //use effect para realizar ciertas operaciones cuando el state cambia
   //cada vez que inicie o agregue o elimine una cita se va a ejecutar este codigo
   useEffect(() => {
-    console.log('Documento listo o algo paso con las citas');
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [citas]);
 
   //Funcion que tome las citas actuales y agregue la nueva
